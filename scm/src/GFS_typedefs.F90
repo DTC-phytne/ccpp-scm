d194d170db38aa5485edf5e4356a44d25a8bc160
Man.Zhang <Man.Zhang@noaa.gov>
2025-07-29 11:50:47 -0600

    save Man's changes for SFSbeta0.1

--- scm/src/GFS_typedefs.F90
+++ scm/src/GFS_typedefs.F90
@@ -5,8 +5,10 @@ module GFS_typedefs
 
    use module_radsw_parameters,  only: topfsw_type, sfcfsw_type
    use module_radlw_parameters,  only: topflw_type, sfcflw_type
-   use h2o_def,                  only: levh2o, h2o_coeff
+!   use h2o_def,                  only: levh2o, h2o_coeff
+!   use module_ozphys,            only: ty_ozphys
    use module_ozphys,            only: ty_ozphys
+   use module_h2ophys,           only: ty_h2ophys
    use module_ccpp_suite_simulator, only: base_physics_process
 
    implicit none
@@ -1620,6 +1622,11 @@ module GFS_typedefs
     type(ty_ozphys) :: ozphys          !< DDT with data needed by ozone physics
     integer         :: levozp          !< Number of vertical layers in ozone forcing data
     integer         :: oz_coeff        !< Number of coefficients in ozone forcing data
+!--- NRL h2o photchemistry physics
+    type(ty_h2ophys) :: h2ophys        !< DDT with data needed by h2o photchemistry physics.
+    integer          :: levh2o         !< Number of vertical layers in stratospheric h2o data.
+    integer          :: h2o_coeff      !< Number of coefficients in stratospheric h2o data.
+
 
 !--- CCPP suite simulator
     logical                                :: do_ccpp_suite_sim  !
@@ -3973,6 +3980,10 @@ module GFS_typedefs
     integer              :: kozpl        = 28       !< File identifier for ozone forcing data
     integer              :: kozc         = 48       !< File identifier for ozone climotology data
 
+!--- NRL h2o photochemistry physics
+    integer              :: kh2oc        = 29       !< File identifier for h2o photochemistry data.
+
+
 !--- CCPP suite simulator
     logical            :: do_ccpp_suite_sim  = .false.
     integer            :: nphys_proc         = 0
@@ -4948,13 +4959,13 @@ module GFS_typedefs
     ! array sizes are compared during model init in GFS_phys_time_vary_init()
     !
     ! from module h2ointerp
-    if (h2o_phys) then
-       levh2o    = 72
-       h2o_coeff = 3
-    else
-       levh2o    = 1
-       h2o_coeff = 1
-    end if
+    !if (h2o_phys) then
+    !   levh2o    = 72
+    !   h2o_coeff = 3
+    !else
+    !   levh2o    = 1
+    !   h2o_coeff = 1
+    !end if
 
     Model%pdfcld            = pdfcld
     Model%shcnvcw           = shcnvcw
@@ -5670,6 +5681,18 @@ module GFS_typedefs
        !--- Climatological ozone
        err_message = Model%ozphys%load_o3clim('global_o3prdlos.f77',kozc)
     end if
+
+        !--- NRL h2o photochemistry physics.
+    if (Model%h2o_phys) then
+       ! Load data for h2o photochemistry physics.
+       err_message     = Model%h2ophys%load('global_h2oprdlos.f77',kh2oc)
+       Model%levh2o    = Model%h2ophys%nlev
+       Model%h2o_coeff = Model%h2ophys%ncf
+    else
+       Model%levh2o    = 1
+       Model%h2o_coeff = 1
+    end if
+
 
 !--- quantities to be used to derive phy_f*d totals
     Model%nshoc_2d         = nshoc_2d
